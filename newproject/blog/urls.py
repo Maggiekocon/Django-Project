ebad811f51790da94b95e2713fd645ca487fb283
Maggiekocon <sunflowerartist4@gmail.com>
2025-10-07 11:00:26 -0500

    Module 7 assignment

--- newproject/blog/urls.py
+++ newproject/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("post/",views.show_post, name='post'),
     path('newpost/', views.add_post, name="addposts"),
     path('post/<int:id>/', views.find_post, name= "find_post"),
+    
     path('updatepost/<int:id>', views.update_post, name= 'updatepost'),
     path('deletepost/<int:id>', views.delete_post, name= 'deletepost')
 ]
